976d254f33afc03825778fa38bb46635596fd8dd
rajivkr8207 <rajiv.kr8207@gmail.com>
2026-04-01 19:52:57 +0530

    fix docker issue

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,7 +10,7 @@ const app = express()
 
 // midleware
 app.use(express.json())
-app.use(morgan('tiny'))
+app.use(morgan('dev'))
 app.use(cookieParser())
 app.use(cors({
     origin: ['http://localhost:5173', 'http://127.0.0.1:5173','http://localhost:8000','http://127.0.0.1:8000'],
