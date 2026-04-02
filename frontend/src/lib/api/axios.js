f9e6b9eab179e8254c20b7b9bbce940b080ad5eb
rajivkr8207 <rajiv.kr8207@gmail.com>
2026-04-02 19:47:47 +0530

    fix origin in frontend

--- frontend/src/lib/api/axios.js
+++ frontend/src/lib/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import ENV from '../../config/env';
 
 export const api = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: ENV.API_BACKEND_URL,
   withCredentials: true,
   timeout: 90000,
   headers: {'X-Custom-Header': 'foobar'}
